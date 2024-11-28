4bb643b4a205d80a62ccb0a97d606b4f7bf5f9fb
siraajul <shirajulislamparvez@gmail.com>
2024-11-28 19:13:00 +0600

    App Route

---
+++ src/app/(User)/profile/page.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import React from "react";
+
+const ProfilePage = () => {
+  return (
+    <>
+      <div className=" bg-white min-h-screen">
+        <h1 className=" text-black text-2xl font-semibold">Profile Page</h1>
+      </div>
+    </>
+  );
+};
+
+export default ProfilePage;
