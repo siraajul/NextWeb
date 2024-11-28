5bcc0125ccf1a66ae8a29739d9f35c718169d0a9
siraajul <shirajulislamparvez@gmail.com>
2024-11-28 19:13:02 +0600

    App Route

---
+++ src/app/products/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import ProductComponents from "@/components/features/product_components";
+import React from "react";
+
+const ProdutPage = () => {
+  return (
+    <>
+      <ProductComponents />
+    </>
+  );
+};
+
+export default ProdutPage;
