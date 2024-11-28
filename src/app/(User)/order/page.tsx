0eb8fce26bb029bf56bdddb9cb896d6e1547a484
siraajul <shirajulislamparvez@gmail.com>
2024-11-28 19:12:57 +0600

    App Route

---
+++ src/app/(User)/order/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import OrderComponents from "@/components/features/order_components";
+import React from "react";
+
+const OrderPage = () => {
+  return (
+    <>
+      <OrderComponents />
+    </>
+  );
+};
+
+export default OrderPage;
