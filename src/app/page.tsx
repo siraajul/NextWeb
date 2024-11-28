f76be12ae92b2bae9377991c0607b4cd3049c654
siraajul <shirajulislamparvez@gmail.com>
2024-11-28 19:12:28 +0600

    App Route

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,88 @@
-import Image from "next/image";
+"use client";
+import axios from "axios";
+import React, { useState } from "react";
 
-export default function Home() {
+const HomePage = () => {
+  const [name, setName] = useState<string>("");
+  const [phone, setPhone] = useState<string>("");
+  const [email, setEmail] = useState<string>("");
+  const [isSubmit, setIsSubmit] = useState<boolean>(false);
+
+  const handleSubmit = async () => {
+    console.log("Trigger");
+    try {
+      const payload = {
+        Name: name,
+        Phone: phone,
+        Email: email,
+      };
+
+      // Make the API call
+      const response = await axios.post(
+        "https://wilful-cricket-badcodersbd-7492dcf3.koyeb.app/auth/login", // Replace with your API URL
+        payload, // Send payload directly
+        {
+          headers: {
+            "Content-Type": "application/json", // Specify JSON content
+          },
+        }
+      );
+
+      // Handle success response
+      console.log("Success:", response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <>
+      <div className=" bg-white min-h-screen p-10 ">
+        <h1 className=" text-2xl font-bold text-green-400 text-center ">
+          Hello World!
+        </h1>
+        <div className="  border border-gray-400 p-5 rounded-sm flex flex-col gap-5 w-[65%]">
+          <label className=" text-black">Name:</label>
+          <input
+            className=" border border-red-500 p-3 rounded-full text-black "
+            type="text"
+            title="Name"
+            placeholder="Enter Your Name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <label className=" text-black">Phone:</label>
+          <input
+            className=" border border-red-500 p-3 rounded-lg text-black "
+            type="text"
+            title="Phone"
+            placeholder="Enter Your Phone Number"
+            value={phone}
+            onChange={(e) => setPhone(e.target.value)}
+          />
+          <label className=" text-black">Email:</label>
+          <input
+            className=" border border-red-500 p-3 rounded-md text-black "
+            type="text"
+            title="Email"
+            placeholder="Enter Your Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <div className="w-full flex justify-center items-center">
+            <button
+              title="Submit"
+              type="button"
+              onClick={() => handleSubmit()}
+              className="border bg-orange-600 rounded-full px-3 py-2 w-[50%] text-white "
+            >
+              Submit
+            </button>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+      </div>
+    </>
   );
-}
+};
+
+export default HomePage;
