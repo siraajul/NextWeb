79406f89b345e7b8d2d78d6c7fc608fbe8eb18ca
siraajul <shirajulislamparvez@gmail.com>
2024-11-28 19:12:47 +0600

    Components

---
+++ src/components/features/order_components/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const OrderComponents = () => {
+  return (
+    <>
+      <div className=" bg-white min-h-screen">
+        <h1 className=" text-black text-2xl font-semibold">Order Components</h1>
+      </div>
+    </>
+  );
+};
+
+export default OrderComponents;
