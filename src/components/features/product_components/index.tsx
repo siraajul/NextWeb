3563598b427d5f53025e94438bd24455b23bcdeb
siraajul <shirajulislamparvez@gmail.com>
2024-11-30 22:57:04 +0600

    Import:--> Next Image

--- src/components/features/product_components/index.tsx
+++ src/components/features/product_components/index.tsx
@@ -1,4 +1,5 @@
 import axios from "axios";
+import Image from "next/image";
 import React, { useEffect, useState } from "react";
 
 type ProductType = {
@@ -46,14 +47,29 @@ const ProductComponents = () => {
         <h1 className=" text-black text-2xl font-semibold">
           Product Components
         </h1>
-        <div className="p-10 flex gap-5">
+        <div className="p-10 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-4 gap-5">
           {products.map((data, index) => (
             <div
               key={index}
-              className=" border text-black border-blue-600 hover:bg-blue-700 hover:text-white cursor-pointer p-5 rounded-xl"
+              className=" border text-black border-blue-600 cursor-pointer p-5 rounded-xl"
             >
+              <div className=" w-full flex justify-center items-center p-2 h-[200px] ">
+                <Image 
+                src={data?.product_prodimage}
+                alt={data?.product_name}
+                height={100}
+                width={100}
+                //fill
+                //style={{  width: "100%", height: "100%" }}
+              
+              />
+              </div>
+              
               <h1>Product Name: {data?.product_name} </h1>
               <h1>Product Price: {data?.product_price} </h1>
+              <div className=" flex justify-center items-center p-2  ">
+              <button className=" bg-orange-600 py-2 px-3 border border-orange-500 rounded-lg text-white hover:bg-green-800">Add To Cart</button>
+              </div>
             </div>
           ))}
         </div>
